da3ed369b42a6e530976773ca6e59dad95ecb8ee
Yunus <yunus.grb130@gmail.com>
2022-11-28 22:14:12 +0300

    Update

--- src/_05_Calismalar/Paytent.java
+++ src/_05_Calismalar/Paytent.java
@@ -3,7 +3,7 @@ package _05_Calismalar;
 public class Paytent {
     public static void main(String[] args) {
 
-        int[] array= {1, 3, 5, 7, 9,11, 13, 15, 17, 19, 21,23,25,29};
+        int[] array= {1, 3, 5, 7, 9,11, 13, 15, 17, 19,23,25,27,29};
         System.out.println(getNumber(array));
 
     }
--- src/_05_Calismalar/Reverse.java
+++ src/_05_Calismalar/Reverse.java
@@ -5,33 +5,24 @@ import java.util.Scanner;
 public class Reverse {
     public static void main(String[] args) {
 
-        String metin = "Herşey Yolunda :)";
-        System.out.println(cevir(metin));
+        String text = "Herşey Yolunda :)";
+        System.out.println(reverse(text));
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Birşeyler yazın:");
-        String text = input.nextLine();
-        reverse(text);
-        System.out.println();
-        reverse(metin);
+        StringBuilder text1=new StringBuilder("Everything is OK!");
+        System.out.println("ReverseText1="+text1.reverse());
+
     }
 
-    public static String cevir(String metin) {
+    public static String reverse(String metin) {
         String tersMetin="";
         if (metin.isEmpty())
             return metin;
         else for (int i = metin.length()-1; i >=0; i--) {
             tersMetin+= metin.charAt(i);
         }
-        System.out.println("tersMetin = " + tersMetin);
         return tersMetin;
           }
-     public static void reverse(String kelime){
-            for (int i = kelime.length() - 1; i >= 0; i--) //i'yi kelime uzunluğu kadar tanımlıyoruz.
-            {
-                System.out.print(kelime.charAt(i));
-            }
 
 
-        }
+
     }
